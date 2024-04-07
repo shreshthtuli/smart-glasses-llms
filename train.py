a49257598cfbbfb58ee56e4f4129f1efa90ca009
Shreshth Tuli <shreshth.tuli@happening.xyz>
2024-04-07 02:52:42 +0100

    feat: add optuna tuning

--- train.py
+++ train.py
@@ -1,20 +1,30 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, SubsetRandomSampler
 
 import lightning as pl
+from lightning.fabric import Fabric, seed_everything
+from lightning.fabric.loggers import TensorBoardLogger
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks import RichProgressBar
+
+from sklearn.model_selection import train_test_split
+
+from optuna.integration import PyTorchLightningPruningCallback
 
 from src.utils import parse_args, get_cuda_device
 from src.dataset import CTCDataset
 from src.llmnet import FCNNet
 
+from rich.progress import track
+
 import pandas as pd
+import optuna
 import shutil
 
 if __name__ == '__main__':
     args = parse_args()
     MODEL = args.model
     BATCH_SIZE = args.batch_size
+    NUM_TRIALS = args.num_trials
     NUM_WORKERS = 4
     DATA_PATH = f'./data/'
     MODEL_SAVE_PATH = f'./models/{MODEL}/'
@@ -27,34 +37,72 @@ if __name__ == '__main__':
     test_dset = pd.read_json(f'{DATA_PATH}/test.jsonl', lines=True)
     train_dset = CTCDataset(train_dset, model='jinaai/jina-embeddings-v2-base-en')
     test_dset = CTCDataset(test_dset, model='jinaai/jina-embeddings-v2-base-en')
-    train = DataLoader(train_dset, 
-                       batch_size=BATCH_SIZE, 
-                       num_workers=NUM_WORKERS, 
-                       persistent_workers=True)
+    train_dset_tmp, val_dset_tmp = train_test_split(train_dset, test_size=0.1)
+    train = DataLoader(train_dset_tmp,
+                    batch_size=BATCH_SIZE,
+                    num_workers=NUM_WORKERS,
+                    persistent_workers=True)
+    val = DataLoader(val_dset_tmp,
+                    batch_size=BATCH_SIZE,
+                    num_workers=NUM_WORKERS,
+                    persistent_workers=True)
     test = DataLoader(test_dset, 
                        batch_size=BATCH_SIZE, 
                        num_workers=NUM_WORKERS, 
                        persistent_workers=True)
 
-    # instantiate model
-    model = eval(MODEL)(params={'input_feat_size': train_dset.get_embedding_size(), 
-                                'num_layers': 3, 
-                                'hidden_feat_size': 512, 
-                                'dropout': 0.1})
+    def objective(trial: optuna.trial.Trial):
+        if args.model == 'FCNNet':
+            input_feat_size = trial.suggest_categorical("input_feat_size", [train_dset.get_embedding_size()])
+            num_layers = trial.suggest_categorical("num_layers", [2, 3, 4])
+            hidden_feat_size = trial.suggest_categorical("hidden_feat_size", [256, 512, 1024])
+            dropout = trial.suggest_categorical("dropout", [0.1, 0.3, 0.5])
+            params = dict(num_layers=num_layers, hidden_feat_size=hidden_feat_size, dropout=dropout,
+                          input_feat_size=input_feat_size)
 
-    # lightning trainer
-    trainer = pl.Trainer(accelerator=get_cuda_device(),
-                            deterministic=True,
-                            devices="auto",
-                            num_sanity_val_steps=0,
-                            default_root_dir=f'./logs/{MODEL}',
-                            max_epochs=200,
-                            enable_checkpointing=False,
-                            callbacks=[EarlyStopping(monitor=METRIC,
+        # instantiate model
+        model = eval(MODEL)(params=params)
+
+        trainer = pl.Trainer(num_sanity_val_steps=0,
+                             max_epochs=200,
+                             accelerator=get_cuda_device(),
+                             enable_checkpointing=False,
+                             default_root_dir=f'./logs/{MODEL}',
+                             callbacks=[EarlyStopping(monitor=METRIC,
+                                                      patience=5, verbose=True,
+                                                      mode="min"),
+                                            RichProgressBar(refresh_rate=3, leave=True),
+                                            PyTorchLightningPruningCallback(trial, monitor="val_loss")]
+                                            )
+        trainer.fit(model, train_dataloaders=train, val_dataloaders=val)
+        results = trainer.validate(model=model, dataloaders=val)[0]
+        if trial.number == 0 or results[METRIC] <= trial.study.best_value:
+            trainer.save_checkpoint(MODEL_SAVE_PATH+f'checkpoint.ckpt')
+        return results[METRIC]
+
+    # do the optuna study
+    study = optuna.create_study(direction="minimize",
+                                storage='sqlite:///db.sqlite3', 
+                                study_name=f'{MODEL}',
+                                load_if_exists=True)
+    if len(study.trials) != NUM_TRIALS:
+        study.optimize(objective, n_trials=NUM_TRIALS)
+
+    # training best model on complete dataset
+    trial = study.best_trial
+    model = eval(MODEL)(params=trial.params)
+    train_complete = DataLoader(train_dset, 
+                       batch_size=BATCH_SIZE, 
+                       num_workers=NUM_WORKERS, 
+                       persistent_workers=True)
+    trainer = pl.Trainer(num_sanity_val_steps=0,
+                         max_epochs=200,
+                         enable_checkpointing=False,
+                         callbacks=[EarlyStopping(monitor=METRIC,
                                                     patience=5, verbose=True,
                                                     mode="min"),
                                     RichProgressBar(refresh_rate=3, leave=True),
                                     ]
                             )
-    trainer.fit(model, train_dataloaders=train, val_dataloaders=test)
+    trainer.fit(model, train_dataloaders=train_complete, val_dataloaders=test)
     trainer.save_checkpoint(MODEL_SAVE_PATH+f'checkpoint.ckpt')
